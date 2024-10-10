e6c61c45351aadb67f1ca614fa9084a8177137de
JuanDaM <juandavidmartinezcastillo5@gmail.com>
2024-10-10 10:47:30 -0500

    Correción de https a http + incorporar un alert

--- api/src/backend/signunp.php
+++ api/src/backend/signunp.php
@@ -5,14 +5,19 @@
     //2 Get Data from register Form
     $email = $_POST['email'];
     $pass = $_POST['passwd'];
+    //el md5 sirve para encriptar contraseña
     $enc_pass = md5($pass);
 
     //3 Query to insert data into userts table
     $query = "INSERT INTO users (email, password) values ('$email', '$enc_pass')";
+    //execute query 
     $result = pg_query($conn, $query);
 
     if ($result){
-     echo  "Registration seccessfully";
+     //echo  "Registration seccessfully";
+     echo "<script>alert('Registration seccessfully')</script>";
+     header('Refresh:0; url=http://127.0.0.1/beta/api/src/login_form.html');
+         //Redireccionar a la página principal al finalizar la registración
        
     }else{
         echo "Registration Failed";
